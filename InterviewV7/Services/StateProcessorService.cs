f1dfdb521dc9d224fe9dce2aaa27e6aa404ed883
Joseph Urban <jsmith2@atsautomation.com>
2023-09-09 12:18:43 -0400

    added data processing and metrics methods

---
+++ InterviewV7/Services/StateProcessorService.cs
@@ -0,0 +1,20 @@
+﻿using InterviewV7.Models;
+
+namespace InterviewV7.Services
+{
+    public class StateProcessorService : IStateProcessorService
+    {
+        public IEnumerable<State> ProcessStates(IEnumerable<State> rawStates)
+        {
+            var stateList = rawStates.ToList();
+
+            return stateList
+                        .Zip(stateList.Skip(1), (current, next) =>
+                        {
+                            current.Duration = next.TimeStamp - current.TimeStamp;
+                            return current;
+                        })
+                        .ToList();
+        }
+    }
+}
